133c2174f37e76b7f9d9c43739506cfa901d5a7a
Unknown <mforall29@gmail.com>
2017-03-19 15:00:06 +0300

    + Proj + struct

---
+++ EZTask/EZTask/Controllers/ToDoController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EZTask
+//
+//  Created by Evgeniy on 19.03.17.
+//  Copyright © 2017 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ToDoController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+}
+
+extension ToDoController
+{
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
+}
