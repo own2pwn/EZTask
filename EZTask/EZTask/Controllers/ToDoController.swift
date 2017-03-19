aae0a98329b2750323fc7fbede3752e0a4067050
Unknown <mforall29@gmail.com>
2017-03-19 21:13:51 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- EZTask/EZTask/Controllers/ToDoController.swift
+++ EZTask/EZTask/Controllers/ToDoController.swift
@@ -11,23 +11,23 @@ import UIKit
 class ToDoController: UIViewController
 {
     // MARK: - Outlets
-
+    
     @IBOutlet weak var toDoTableView: UITableView!
-
+    
     // MARK: - Properties
-
+    
     fileprivate let toDoCellIdentifier = "idToDoCell"
-
+    
     var numOfItems = 7
-
+    
     // MARK: - Life cycle
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        
     }
-
+    
 }
 
 // MARK: - Extensions
@@ -43,22 +43,22 @@ extension ToDoController: UITableViewDelegate
             print("More")
         }
         more.backgroundColor = .lightGray
-
+        
         let favorite = UITableViewRowAction(style: .normal, title: "Change list")
         { action, index in
             print("Change list")
         }
         favorite.backgroundColor = .orange
-
+        
         let share = UITableViewRowAction(style: .normal, title: "Snooze")
         { action, index in
             print("Snooze")
         }
         share.backgroundColor = .blue
-
+        
         return [share, favorite, more]
     }
-
+    
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool { return true }
 }
 
@@ -68,16 +68,16 @@ extension ToDoController
     {
         let checkView = KZSwipeTableViewCell.viewWithImage(#imageLiteral(resourceName: "checkMarkIcon"))
         let greenColor = UIColor(red: 85.0 / 255.0, green: 213.0 / 255.0, blue: 80.0 / 255.0, alpha: 1.0)
-
+        
         let crossView = KZSwipeTableViewCell.viewWithImage(#imageLiteral(resourceName: "crossIcon"))
         let redColor = UIColor(red: 232.0 / 255.0, green: 61.0 / 255.0, blue: 14.0 / 255.0, alpha: 1.0)
-
+        
         let clockView = KZSwipeTableViewCell.viewWithImage(#imageLiteral(resourceName: "watchesIcon"))
         let yellowColor = UIColor(red: 254.0 / 255.0, green: 217.0 / 255.0, blue: 56.0 / 255.0, alpha: 1.0)
-
+        
         let listView = KZSwipeTableViewCell.viewWithImage(#imageLiteral(resourceName: "sticksIconIcon"))
         let brownColor = UIColor(red: 206.0 / 255.0, green: 149.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
-
+        
         if let bgView = self.toDoTableView.backgroundView
         {
             if let bgColor = bgView.backgroundColor
@@ -85,52 +85,31 @@ extension ToDoController
                 cell.settings.defaultColor = bgColor
             }
         }
-
-        if indexPath.row != (numOfItems - 1)
-        {
-            cell.textLabel?.text = "Task"
-            cell.detailTextLabel?.text = "Subtitle"
-            cell.settings.secondTrigger = 0.66
-
-            // TODO: make deletion on right side & change secondTrigger to a smaller value
-
-            // TODO: add gradient & animation on color changing
-
-            cell.setSwipeGestureWith(checkView, color: greenColor, mode: .exit, state: .state1, completionBlock: { (cell, state, mode) -> Void in
-                print("Marked task as done")
-
-                self.deleteCell(cell)
-            })
-
-            cell.setSwipeGestureWith(crossView, color: redColor, mode: .switch, state: .state2, completionBlock: { (cell, state, mode) -> Void in
-                print("Deleting task")
-            })
-
-            cell.setSwipeGestureWith(clockView, color: yellowColor, mode: .switch, state: .state3, completionBlock: { (cell, state, mode) -> Void in
-                print("Snoozing task")
-            })
-
-            cell.setSwipeGestureWith(listView, color: brownColor, mode: .switch, state: .state4, completionBlock: { (cell, state, mode) -> Void in
-                print("Changing list of task")
-            })
-        }
-        else
-        {
-            cell.textLabel?.text = "Task"
-            cell.detailTextLabel?.text = "Subtitle"
-
-            cell.setSwipeGestureWith(checkView, color: greenColor, mode: .exit, state: .state1, completionBlock: { (cell, state, mode) -> Void in
-                print("Marked task as done")
-            })
-
-            cell.setSwipeGestureWith(crossView, color: redColor, mode: .switch, state: .state2, completionBlock: { (cell, state, mode) -> Void in
-                print("Deleting task")
-            })
-
-            cell.setSwipeGestureWith(clockView, color: yellowColor, mode: .none, state: .state3)
-        }
+        
+        cell.textLabel?.text = "Task"
+        cell.detailTextLabel?.text = "Subtitle"
+        cell.settings.secondTrigger = 0.66
+        
+        // TODO: make deletion on right side & change secondTrigger to a smaller value
+        
+        // TODO: add gradient & animation on color changing
+        
+        cell.setSwipeGestureWith(checkView, color: greenColor, mode: .exit, state: .state1, completionBlock: { (cell, state, mode) -> Void in
+            print("Marked task as done")
+            
+            self.deleteCell(cell)
+        })
+        
+        cell.setSwipeGestureWith(clockView, color: yellowColor, mode: .switch, state: .state3, completionBlock: { (cell, state, mode) -> Void in
+            print("Snoozing task")
+        })
+        
+        cell.setSwipeGestureWith(listView, color: brownColor, mode: .switch, state: .state4, completionBlock: { (cell, state, mode) -> Void in
+            print("Changing list of task")
+        })
+        
     }
-
+    
     func deleteCell(_ cell: KZSwipeTableViewCell)
     {
         numOfItems -= 1
@@ -146,7 +125,7 @@ extension ToDoController: UITableViewDataSource
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         // TODO: change
-
+        
         return numOfItems
     }
     
@@ -154,16 +133,16 @@ extension ToDoController: UITableViewDataSource
     {
         return 2
     }
-
+    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = KZSwipeTableViewCell(style: .subtitle, reuseIdentifier: toDoCellIdentifier)
-
+        
         configureCell(cell, indexPath: indexPath)
-
+        
         return cell
     }
-
+    
 }
 
 // MARK: Other
