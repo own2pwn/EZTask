86fbc74b9dfd67ad158e48428ca45cc71de66443
Unknown <mforall29@gmail.com>
2017-03-20 22:18:32 +0300

    + ToDoCell

---
+++ EZTask/EZTask/View/ToDoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoCell.swift
+//  EZTask
+//
+//  Created by Evgeniy on 20.03.17.
+//  Copyright © 2017 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
